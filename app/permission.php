18c0da28d17252aee6fec02a7bc9e1e1d28344ae
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2020-05-19 22:10:53 +0600

    relationship between permission-user and permission-role added

--- app/permission.php
+++ app/permission.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class permission extends Model
 {
-    //
+    public function roles() {
+      return $this->belongsToMany(Role::class, 'roles_permissions');
+    }
+
+    public function users() {
+      return $this->belongsToMany(User::class, 'users_permissions');
+    }
 }
