18c1d57c2b7100da23952a615073977f0b04af6b
Mehedi Hassan Durjoi <m.h.durjoi@gmail.com>
2020-05-20 01:01:53 +0600

    Middleware added

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 
 Route::get('/roles', 'PermissionController@Permission');
+
+Route::group(['middleware' => 'role:developer'], function() {
+  Route::get('/admin', function(){
+    return 'Welcome Admin';
+  });
+});
