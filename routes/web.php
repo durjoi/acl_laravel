1f2594df1f8fb34f310d3503d8f33489bdb50645
mehedi-durjoi <m.h.durjoi@gmail.com>
2020-05-28 19:26:44 +0600

    Bug fixed

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/roles', 'PermissionController@Permission');
 
+Route::get('/test', function(){
+  return "Test Route";
+});
+
 Route::group(['middleware' => 'role:developer'], function() {
   Route::get('/admin', function(){
     return 'Welcome Admin';
